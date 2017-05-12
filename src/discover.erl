acba286cf4393de5e8b91a313682d8a65f549f03
danielgabin <daniel.gabin@udc.es>
2017-05-12 17:04:18 +0200

    Discover sin filtro

--- src/discover.erl
+++ src/discover.erl
@@ -33,53 +33,46 @@ start()->
 init(Port,L) ->
 	{ok, Socket} = gen_udp:open(Port, [binary, {active,true}]),
 	io:format("Server opened socket:~p~n",[Socket]),
-	loop(Socket,L, Port, []).
+	loop(Socket,L, Port).
 
 
-loop(Socket,L, FilePort, IpList) ->
+loop(Socket,L, FilePort) ->
     inet:setopts(Socket, [{active, once}]),
     receive
         {udp, Socket, IP, Port, Msg} ->
-        	case filter:filter(IP, IpList) of
-					access_allowed ->
-						io:format("Access allowed",[]),
-		            	case erlang:binary_to_term(Msg) of	
-			            	poll_request -> util:send(Socket, IP, Port, erlang:term_to_binary(L)),
-			            					loop(Socket, L, FilePort, filter:update_list(IP, IpList));
-			            	{register, PollName} ->   case check_poll_name(L, PollName) of 
-			            								true -> 
-					            							util:receive_file(PollName, Socket),
-					            							util:send(Socket,IP,Port,erlang:term_to_binary({ok,registered})),
-					            							loop(Socket, [{IP,Port,PollName}|L], FilePort, filter:update_list(IP, IpList));
-					            						_ ->  
-					            							util:send(Socket, IP, Port, erlang:term_to_binary(name_not_avaliable)),
-					            							loop(Socket, L, FilePort, filter:update_list(IP, IpList))
-					            					  end;
-					        {delete, PollName} -> case check_poll_name(L,PollName) of 
-					        						false -> 
-					        							{PollIP,PollPort,PollName} = lists:keyfind(PollName,3,L),
-					        							if IP == PollIP ->
-					        									util:send(Socket, IP, Port, erlang:term_to_binary(deleted)),
-					        									loop(Socket,lists:delete({PollIP,PollPort,PollName},L),FilePort,  filter:update_list(IP, IpList));
-					        								true -> 
-					        									util:send(Socket, IP, Port, erlang:term_to_binary(owner_error)),
-					            							  	loop(Socket, L, FilePort,  filter:update_list(IP, IpList))
-					            						end;
-					            					_ ->
-					            						util:send(Socket, IP, Port, erlang:term_to_binary(non_existing_name)),
-					            						loop(Socket, L, FilePort,  filter:update_list(IP, IpList))
-					            				  end;
-			                {public_key, PollName} -> 
-			                            util:send_file(IP, Port, ?KEYFOLDER++PollName++".pub"),
-			                            loop(Socket, L, FilePort,  filter:update_list(IP, IpList));
-					        {renew} ->  NewL = update(L, IP, Port, []),
-					        			util:send(Socket, IP, Port, erlang:term_to_binary(port_changed)),
-					        			loop(Socket, NewL, FilePort,  filter:update_list(IP, IpList))
-		            	end;
-		            access_denied ->io:format("Access denied",[]), loop(Socket, L, FilePort, filter:update_list(IP, IpList))
-		    end;
-		{vaciar_lista} ->
-				loop(Socket,L, FilePort, [])
+        	case erlang:binary_to_term(Msg) of	
+            	poll_request -> util:send(Socket, IP, Port, erlang:term_to_binary(L)),
+            					loop(Socket, L, FilePort);
+            	{register, PollName} ->   case check_poll_name(L, PollName) of 
+            								true -> 
+		            							util:receive_file(PollName, Socket),
+		            							util:send(Socket,IP,Port,erlang:term_to_binary({ok,registered})),
+		            							loop(Socket, [{IP,Port,PollName}|L], FilePort);
+		            						_ ->  
+		            							util:send(Socket, IP, Port, erlang:term_to_binary(name_not_avaliable)),
+		            							loop(Socket, L, FilePort)
+		            					  end;
+		        {delete, PollName} -> case check_poll_name(L,PollName) of 
+		        						false -> 
+		        							{PollIP,PollPort,PollName} = lists:keyfind(PollName,3,L),
+		        							if IP == PollIP ->
+		        									util:send(Socket, IP, Port, erlang:term_to_binary(deleted)),
+		        									loop(Socket,lists:delete({PollIP,PollPort,PollName},L),FilePort);
+		        								true -> 
+		        									util:send(Socket, IP, Port, erlang:term_to_binary(owner_error)),
+		            							  	loop(Socket, L, FilePort)
+		            						end;
+		            					_ ->
+		            						util:send(Socket, IP, Port, erlang:term_to_binary(non_existing_name)),
+		            						loop(Socket, L, FilePort)
+		            				  end;
+                {public_key, PollName} -> 
+                            util:send_file(IP, Port, ?KEYFOLDER++PollName++".pub"),
+                            loop(Socket, L, FilePort);
+		        {renew} ->  NewL = update(L, IP, Port, []),
+		        			util:send(Socket, IP, Port, erlang:term_to_binary(port_changed)),
+		        			loop(Socket, NewL, FilePort)
+        	end
     end.
 
 
